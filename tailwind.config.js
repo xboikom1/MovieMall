4f793b3ea6d9e24806005547e0207e603186f2de
Boiko_Maksym <xboikom1@stuba.sk>
2026-03-16 19:08:55 +0100

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        screens: {
+            tablet: '768px',
+            desktop: '1024px'
+        },
+        extend: {
+            colors: {
+                bg: '#0f172a',
+                text: '#f9fafb',
+                rose: '#f43f5e'
+            }
+        }
+    }
+};
